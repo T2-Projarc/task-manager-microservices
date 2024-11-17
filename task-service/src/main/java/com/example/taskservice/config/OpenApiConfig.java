47b10caf1fd6e5e7097a487634f1729b5a8f059f
Lucas Ramon Schnorrenberger <lucas.ramon@edu.pucrs.br>
2024-11-17 18:48:00 -0500

    "[Q] Adicionar documentação com Spring Doc #4
    Closes #4
    
    Adicionado suporte à geração automática de documentação de API usando Spring Doc para todos os microsserviços.
    
    - Spring Doc configurado em todos os microsserviços.
    - Documentação acessível através de /swagger-ui.html.
    - Documentação atualizada com todos os endpoints disponíveis."

---
+++ task-service/src/main/java/com/example/taskservice/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.example.taskservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API do Serviço de Task")
+                        .description("Documentação da API do serviço de Task")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Integrantes: Esthevan Pereira, Lucas Ramon, Maria Eduarda Maia, Maurício Krziminski")
+                                .url("https://github.com/T2-Projarc/task-manager-microservices")));
+    }
+}
