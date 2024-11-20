4f68bb03b49b3fd803333e083e162e069de0360c
MauricioKrziminski <maumausk2015@gmail.com>
2024-11-20 18:06:35 -0300

    feat: add NonNull annotation to addCorsMappings method in WebConfig classes

--- gateway-service/src/main/java/com/example/gatewayservice/config/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/JwtAuthenticationFilter.java
@@ -3,10 +3,8 @@ package com.example.gatewayservice.config;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
-import reactor.core.publisher.Mono;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 
@@ -32,8 +30,8 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
             }
 
             try {
-                String token = authHeader.substring(7); 
-                jwtDecoder.decode(token); 
+                String token = authHeader.substring(7);
+                jwtDecoder.decode(token);
             } catch (Exception e) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
